577f779ec8815a07e04372fffb1d5f7be1fd812d
Apple <Apple@Apples-MacBook-Air.local>
2015-12-24 14:21:04 +0700

    first commit

--- i am rich/ViewController.m
+++ i am rich/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *iamrich;
+@property (weak, nonatomic) IBOutlet UIImageView *ruby;
+
 
 @end
 
@@ -17,11 +20,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.iamrich.alpha = 0;
+    self.ruby.alpha = 0;
 }
-
+- (void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [UIView animateWithDuration:4 animations:^{
+        self.ruby.alpha = 1;
+        
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:3 animations:^{
+            self.iamrich.center =  CGPointMake(self.iamrich.center.x, 360);
+            self.iamrich.alpha = 1;
+        }];
+    }];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 @end
